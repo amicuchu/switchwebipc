0ab1083563b57fbdcbafa215629fc503da83e1aa
Amicuchu <amicuchu@yahoo.co.jp>
2020-01-26 01:40:26 +0100

    First iteration of web frameork

--- source/ExampleHandler.hpp
+++
@@ -1,9 +0,0 @@
-#include "WebSessionHandler.hpp"
-
-class ExampleHandler : public WebSessionHandler{
-public:
-    void giveWebSession(std::weak_ptr<WebSession> webSession) override;
-    void handleMessage(const std::vector<u8> &data) override;
-private:
-    std::weak_ptr<WebSession> webSession;
-};
